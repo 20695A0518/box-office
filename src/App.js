2b37cd47689474bdccd90adfef22c7d6cb814a37
20695A0518 <kundharapuvasudeva@gmail.com>
2022-08-14 11:25:36 +0530

    Well we are doing well

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-    return <div > hi < /div>;
+    return ( <
+        Routes >
+        <
+        Route exact path = "/" > { ' ' }
+        This home page { ' ' } <
+        /Route>{' '} <
+        Route path = "/started" > started the pages < /Route>{' '} <
+        Route > this 404 pages < /Route> <
+        /Routes>
+    );
 }
 
 export default App;
